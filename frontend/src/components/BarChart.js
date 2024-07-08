90eba3db5b062896b89ed30dc63059c73d8d927a
projjwalmallik <76209495+projjwalmallik@users.noreply.github.com>
2024-07-08 17:46:13 +0530

    removed bug

--- frontend/src/components/BarChart.js
+++ frontend/src/components/BarChart.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { Chart, registerables } from "chart.js";
-import { Bar } from "react-chartjs-2";
 
 Chart.register(...registerables);
 
